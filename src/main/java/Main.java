f7a8b323ca869b4bfcccde33c41ca21aafe83819
Maggie <maggie@Zipcoders-MacBook-Pro-85.local>
2023-06-28 17:17:51 -0400

    Code Added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+        Scanner input = new Scanner(System.in);
+        String userInput = "";
+
+        System.out.println("Please enter your name");
+
+        userInput = input.nextLine();
+
+        if (Objects.equals(userInput, "Alice")){
+            System.out.println("Hello, " + userInput);
+        }
+        else if (Objects.equals(userInput, "Bob")){
+            System.out.println("Hello, " + userInput);
+        }
+        else {
+            System.out.println("Thanks");
+        }
 
     }
 }
